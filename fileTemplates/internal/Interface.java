be72ec1f8429f931c2e497f1efdf5295ab6dd0d3
zhouxinghuai <zhouxinghuai@yunzhangfang.com>
2019-08-08 10:45:34 +0800

    IU-2019.1.3 <JasonZh@sternberg Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Default copy.xml, Interface.java, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, keymap.xml, gradle.run.settings.xml, code.style.schemes.xml, mavenVersion.xml, MyLiveTemplate.xml, GrepConsole.xml, Default.xml, docker.xml, plain.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, alibaba-toolkit-tips.xml, ide.general.xml, git.xml, ide-features-trainer.xml, lombok-plugin.xml, GoogleStyle.xml, generic-key-value-store.xml, ui.lnf.xml, filetypes.xml, javaeeExternalResources.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, templates.xml, goLibraries.xml, MyDarcula.icls, go.sdk.xml, hg.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, intentionSettings.xml, web-browsers.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, usageView.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) ${YEAR}. 南京云帐房网络科技有限公司 版权所有
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @Description: 
+ * @Author: zhouxinghuai
+ * @CreateDate: ${YEAR}/${MONTH}/${DAY} ${TIME}
+ * @UpdateDate: ${YEAR}/${MONTH}/${DAY} ${TIME}
+ * @Version: 1.0
+ * @UpdateUser:
+ * @UpdateRemark:
+ */
+public interface ${NAME} {
+    
+}
